da556262f5966d61268d6aef41337483e27e2194
ErTreK8 <ericgarciagispert@gmail.com>
2024-10-18 16:59:27 +0200

    Creacio Beta Menu

---
+++ Scripts/Menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://escenas/Game.tscn")
+
+
+func _on_Options_pressed():
+	get_tree().change_scene("res://escenas/Options.tscn")	
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
+	
