d0f1fb964c02235cf4cc7739d35217f4a320b882
ErTreK8 <ericgarciagispert@gmail.com>
2024-10-21 19:46:12 +0200

    Creacio de Sprite de Cartes

---
+++ Scripts/Carta.gd
@@ -0,0 +1,3 @@
+extends Sprite
+
+var img = "res://Assets/cartas_sueltas/Clubs/tile000.png"
---
+++ escenas/Gamse.gd
@@ -0,0 +1,4 @@
+extends Node2D	
+func _ready():
+	$Sprite.texture = load("res://Assets/cartas_sueltas/Clubs/tile004.png")
+
