83c2ead2e68cd502f11ae8060ed5229fdd4eab1b
ErTreK8 <ericgarciagispert@gmail.com>
2024-11-07 20:12:36 +0100

    Animacion Robar Cartas IA

--- escenas/Gamse.gd
+++ escenas/Gamse.gd
@@ -78,46 +78,78 @@ var Baraja = [
 	"res://Assets/cartas_sueltas/Clubs/tile012.png"
 ]
 var CartasSacadas=[]
-
+var interfaz_players = []
 
 func _ready():
 	randomize()
 	# ------------------------------------------------
 	# Instanciar los nombres y fichas de jugadores
 	# ------------------------------------------------
-	for i in range(numPlayer-1):
+	for i in range(numPlayer - 1):
 		var player_card_instance = player_card_scene.instance()
 
 		# Asignar nombre y chips a los labels
-		player_card_instance.get_node("Background/Player_Name").text = playerName[i+1]
-		player_card_instance.get_node("Background/Chips").text = str(PlayerChips[i+1])
+		player_card_instance.get_node("Background/Player_Name").text = playerName[i + 1]
+		player_card_instance.get_node("Background/Chips").text = str(PlayerChips[i + 1])
 
 		# Ubicar el cartel en la posición correspondiente del PathFollow2D
 		var path_follow = path_follows[i]
 		player_card_instance.rect_position = path_follow.position
 		player_card_instance.rect_scale = Vector2(0.75, 0.75)
-		
-		# Añadir la instancia a la escena
-		add_child(player_card_instance)
 
-	# ------------------------------------------------
-	# Crear cartas en las ubicaciones especificadas
-	# ------------------------------------------------
-	for i in range(numPlayer*2):
-		var card_instance = card_scene.instance()
-		
-		# Elegir una carta aleatoria de la baraja
-		var card_texture = cojeCarta()
-		if(i<2):
-			card_instance.get_node("Img").texture = card_texture
-			var card_position = Card_positions[i]
-			card_instance.rect_position = card_position.position
-			card_instance.rect_scale = Vector2(1.5, 1.5)
-			yield(get_tree().create_timer(1), "timeout")
-			add_child(card_instance)
-		#else:
-			#hacer que el codigo muestre la Behind_Card1 se espere 1 sec y que en la siguiente ronda de i muestre Behind_Card2
-		playerCards.append(card_texture)
+		# Añadir la instancia a la escena y al array
+		add_child(player_card_instance)
+		interfaz_players.append(player_card_instance)  # Agregar al array
+
+	#Robar Cartas Y enseñarlas poco a poco
+	startGame()
+	
+	#Empezar Juego
+func startGame():
+		mostrar_cartas_secuencialmente()
+		ordreJugadors()
+
+func ordreJugadors():
+	#miau
+	a
+
+# Robar Las Cartas God
+func mostrar_cartas_secuencialmente():
+	# Primera carta del jugador principal
+	yield(robar_carta_principal(0), "completed")
+	# Segunda carta del jugador principal
+	yield(robar_carta_principal(1), "completed")
+	
+	# Cartas para los bots
+	for i in range(numPlayer - 1):
+		yield(robar_2_carta_bot(i), "completed")
+
+
+func robar_carta_principal(card_index):
+	yield(get_tree().create_timer(0.5), "timeout")  # Esperar 1 segundo entre cartas	
+	var card_instance = card_scene.instance()
+	var card_texture = cojeCarta()
+	card_instance.get_node("Img").texture = card_texture
+	card_instance.rect_position = Card_positions[card_index].position
+	card_instance.rect_scale = Vector2(1.5, 1.5)
+	add_child(card_instance)
+	playerCards.append(card_texture)
+
+
+func robar_2_carta_bot(bot_index):
+	yield(get_tree().create_timer(0.5), "timeout")  # Esperar 1 segundo	
+	var bot_instance = interfaz_players[bot_index]
+	
+	# Primera carta oculta del bot
+	var card_texture_1 = cojeCarta()
+	bot_instance.get_node("Background/Behind_Card1").show()
+	playerCards.append(card_texture_1)
+	yield(get_tree().create_timer(0.5), "timeout")  # Esperar 1 segundo
+	
+	# Segunda carta oculta del bot
+	var card_texture_2 = cojeCarta()
+	bot_instance.get_node("Background/Behind_Card2").show()
+	playerCards.append(card_texture_2)
 
 
 func cojeCarta():
